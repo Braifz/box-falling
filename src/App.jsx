b8822c69286af7304aeb920f4062242463443639
braifz <braian94lp@gmail.com>
2022-06-10 00:55:39 -0300

    add sphere and plane components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Physics } from '@react-three/cannon';
-import { OrbitControls } from '@react-three/drei';
+import { OrbitControls, Stars, ContactShadows } from '@react-three/drei';
 import ThreeScene from './components/ThreeScene';
 import Plane from './components/Plane';
 import Sphere from './components/Sphere';
@@ -12,15 +12,30 @@ const App = () => {
     <div className="App">
       <h1>hello :D</h1>
       <ThreeScene>
+        <ambientLight />
+        <directionalLight />
+        <Stars />
+        <ContactShadows
+          position={[0, 0, 0]}
+          opacity={1}
+          scale={550}
+          blur={1}
+          far={170}
+          color="white"
+        />
         <Physics>
-          <Plane />
-          <Sphere />
-          <Sphere />
-          <Sphere />
-          <Sphere />
+          <Plane rotation={[-Math.PI / 2, 0, 0]} />
+          <Sphere position={[0, 20, 10]} />
+          <Sphere position={[0, 40, 10]} />
+          <Sphere position={[0, 60, 10]} />
+          <Sphere position={[0, 80, 10]} />
+          <Sphere position={[0, 100, 10]} />
+          <Sphere position={[0, 120, 10]} />
+          <Sphere position={[0, 140, 10]} />
+          <Sphere position={[0, 160, 10]} />
         </Physics>
         <Camera />
-        <OrbitControls />
+        <OrbitControls autoRotate />
       </ThreeScene>
     </div>
   );
